f0dcd6c64c2f777c4e8dda8fcfa94af8c549ff58
Steven Clerc <clerc.steven@gmail.com>
2021-12-27 23:37:25 +0100

    feat: improve hastag handling

--- tags/src/components/tags.vue
+++ tags/src/components/tags.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="input-container">
     <h1>{{title}}</h1>
-    <input type="text" v-model="customText">
-    <div v-for="tag in tagList" :key="tag">
-        <span v-on:click="selectTag(tag)">{{tag}}</span>
+    <div type="text" id="text" contenteditable="true" @input="message"></div>
+    <div v-show="showList" id="tag-list">
+      <div class="tag" v-on:click="selectTag(tag)" v-for="tag in tagList" :key="tag">
+          <span>#{{tag}}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 
-import {topNearestWords} from '../services/searchService';
+// import {topNearestWords} from '../services/searchService';
 
 // eslint-disable-next-line
 const specialCharactersList = new RegExp(/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/);
@@ -21,42 +23,138 @@ export default {
       return {
           title: 'Tags test',
           tagList: [],
-          customText: '',
+          wordArray: [],
+          givenList: [
+            'liver',
+            'pain',
+            'pain',
+            'right',
+            'left',
+            'pancreas',
+            'kidney',
+            'brain',
+            'severe_pain',
+            'tumour',
+            'cancer',
+            'MRI',
+            'CT',
+            'male',
+            'female',
+            'bone',
+            'shoulder',
+            'hip',
+            'XRAY',
+            'knee',
+            'spine',
+            'head',
+            'abdomen',
+            'contrast',
+            'fragment',
+            'detached',
+            'injury',
+            'torn',
+            'rotator',
+            'cuff',
+            'abdominal',
+            'dilatation',
+          ],
       };
   },
   methods: {
       selectTag(tag) {
-        const hastageLastPoition = this.customText.lastIndexOf('#');
-        this.customText = this.customText.slice(0, hastageLastPoition + 1);
-        this.customText += `${tag} `;
+        let text = this.$el.querySelector('#text').innerHTML;
+        // this.getCaretPosition();
+        const hastageLastPoition = text.lastIndexOf('#');
+        text = text.slice(0, hastageLastPoition) + `<span style="color:deepskyblue;">#${tag}</span>&nbsp;`;
+        this.$el.querySelector('#text').innerHTML = text;
         this.cleanTagList();
+        this.resetCaretPosition();
+      },
+      getCaretPosition() {
+        // const el = this.$el.querySelector('#text-zone')
+        const range = document.createRange();
+        const sel = window.getSelection();
+
+        range.collapse(true);
+        
+        console.log('range', range);
+        console.log('sel', sel);
+      },
+      clearText() {
+        let text = this.$el.querySelector('#text').innerHTML;
+        text.replaceAll('<span style="color:deepskyblue;">&nbsp;');
+        text.replaceAll('</span>', '');
+      },
+      resetCaretPosition() {
+        const el = this.$el.childNodes[1];
+        const range = document.createRange();
+        const sel = window.getSelection();
+
+        range.setStartAfter(el, el.childNodes.length - 1); // el.innerHTML.length
+        range.collapse(true);
+        
+        sel.removeAllRanges();
+        sel.addRange(range);
+        // left => offsetLeft top => offsetTop + offsetHeight
       },
       cleanTagList() {
           this.tagList = [];
-      }
-  },
-  watch: {
-    async customText(newText) {
-        //slice le dernier hastag => fin du text
-        // si y a pas de caractère spécial launch request et afiche dropdown
-        const hastageLastPoition = newText.lastIndexOf('#');
-        this.cleanTagList();
-        if (hastageLastPoition !== - 1) {
-            const newTags = newText.slice(hastageLastPoition + 1, newText.length);
-            if (newTags.length > 0 && !specialCharactersList.test(newTags)) {
-                try {
-                    this.tagList = await topNearestWords(newTags);            
-                } catch(error) {
-                    console.log(error);
-                }
+      },
+      message: function(el) {
+        this.wordArray = el.target.innerHTML.split(' ');
+        const reversedArray = this.wordArray.reverse();
+        for (let i = 0; i < reversedArray.length; i++) {
+          if (reversedArray[i].includes('#')){
+            const text = reversedArray[i];
+            reversedArray[i] = `<span style="color:deepskyblue;">${text}</span>` + ' ';
+            const newTags = text.slice(1, text.length);
+            if (newTags.length > 0) {
+              this.tagList = this.givenList.filter((elem) => elem.includes(newTags));
+              this.showList = true;
+
+              const textZone = el.childNodes[el.childNodes.length - 1];
+              const listBlock = this.$el.querySelector('#tag-list');
+              listBlock.style.left = textZone.offsetLeft;
+              listBlock.style.top = textZone.offsetTop + textZone.offsetHeight;
+            } else {
+              this.showList = false;
             }
+            // el.target.innerHTML = reversedArray.reverse().join(' ');
+            break;
+          }
         }
-    }
-  }
+      },
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="css" scoped>
+
+.input-container {
+  position: relative;
+  max-width: 500px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+#text {
+  border: 1px solid gray;
+  min-height: 200px;
+  text-align: left;
+  padding: 5px;
+}
+
+#tag-list {
+  position: absolute;
+  max-width: 100px;
+  min-height: 100px;
+  border: 1px solid;
+}
+
+.tag {
+  font-weight: bold;
+  cursor: pointer;
+}
 
 </style>
