4bd3abe06a86f88f37014212401d7e197fcee0ab
Clerc Steven <clerc.steven@gmail.com>
2022-09-29 14:54:58 +0200

    wip package and transfer wip file

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import pumpkinBanner from "./src/components/pumpkin-banner.vue";
+import pumpkinCrumb from "./src/components/pumpkin-crumb.vue";
+import pumpkinTags from "./src/components/pumpkin-tags.vue";
+import pumpkinSummary from "./src/components/pumpkin-summary.vue";
+import pumpkinIntersectionSolo from "./src/components/pumpkin-intersection-solo.vue";
+import pumpkinModal from "./src/components/pumpkin-modal.vue";
+import pumpkinStepper from "./src/components/pumpkin-stepper.vue";
 
-export function pumpkinExport() {
-  console.log("This is a message from the demo package");
-}
 
-export function pumpkinComponents() {
-  return {
-    pumpkinBanner,
-  };
+// function pumpkinExport() {
+//   console.log("This is a message from the demo package");
+// }
+
+export default function install(Vue, options) {
+  Vue.component("pumpkinBanner", pumpkinBanner);
+  Vue.component("pumpkinCrumb", pumpkinCrumb);
+  Vue.component("pumpkinTags", pumpkinTags);
+  Vue.component("pumpkinSummary", pumpkinSummary);
+  Vue.component("pumpkinIntersectionSolo", pumpkinIntersectionSolo);
+  Vue.component("pumpkinModal", pumpkinModal);
+  Vue.component("pumpkinStepper", pumpkinStepper);
+
+  //  pumpkinExport,
 }
---
+++ src/components/pumpkin-teleport.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+// BESOIN DE REFLECTION
+<template>
+  <Teleport to="body" :disabled="!props.teleport">
+    <div
+      v-if="props.showModal"
+      :class="{ 'pumpkin-overlay': overlay && props.showModal }"
+      @click.self="closeModal"
+    >
+      <div class="pumpkin-modal">
+        <header class="pumpkin-header">
+          <slot name="header"></slot>
+          <h1>{{ props.contentTitle }}</h1>
+          <button class="pumpkin-close" @click="closeModal">&#10006;</button>
+        </header>
+        <main class="pumpkin-body">
+          <slot name="body"></slot>
+          <p>{{ props.contentBody }}</p>
+        </main>
+        <footer class="pumpkin-footer">
+          <slot name="footer"></slot>
+        </footer>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style lang="css" scoped></style>
